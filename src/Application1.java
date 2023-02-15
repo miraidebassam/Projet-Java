c476c04821981528f3340a8ccde82d616442e668
Miraide Augusto Bassam <77417301+miraidebassam@users.noreply.github.com>
2023-02-15 15:36:10 +0000

    Update Application1.java

--- src/Application1.java
+++ src/Application1.java
@@ -3,7 +3,7 @@ import models.Etudiant;
 public class Application1 {
 
     public static void main(String[] args) {
-        Etudiant etudiant = new Etudiant("KOUNASSO", "Lazare Razacki", 'M', 12, 1234, "MP-ISI");
+        Etudiant etudiant = new Etudiant("BASSAM", "Miraide", 'F', 15, 1234, "MP-ISI");
         etudiant.sePresenter();
     }
 }
